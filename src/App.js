8aaf49da65fa892aacf1a27f4833b1e94583e284
Ana Kwon <anakwon@gmail.com>
2021-05-28 12:13:46 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './component/layout/Navbar';
+import Index from './component/layout/Index';
+import Lyrics from './component/tracks/Lyrics';
+
+import { Provider } from './context';
+
+class App extends Component {
+  render () {
+    return (
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path ="/" component={Index} />
+                <Route exact path ="/lyrics/track/:id" component={Lyrics} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    )
+  }
 }
 
 export default App;
